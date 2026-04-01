823678d657a95ff742d005c301bb6711e2b513f6
Van Tran <vantran15122004@gmail.com>
2026-04-01 23:00:34 +0700

    Initial Spring Boot project

---
+++ src/test/java/com/example/soundscape_app/SoundscapeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soundscape_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundscapeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
