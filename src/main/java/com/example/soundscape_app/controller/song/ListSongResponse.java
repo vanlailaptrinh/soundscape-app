1db3055285bac577e71d63827e2db83b9d93fef4
Ha Truong <hacaotantruong0@gmail.com>
2026-05-23 20:59:52 +0700

    tao lop ListSongResponse

---
+++ src/main/java/com/example/soundscape_app/controller/song/ListSongResponse.java
@@ -0,0 +1,18 @@
+package com.example.soundscape_app.controller.song;
+
+import com.example.soundscape_app.enums.SongStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListSongResponse {
+    private Long id;
+    private String title;
+    private String artistEmail;
+    private SongStatusEnum status;
+}
