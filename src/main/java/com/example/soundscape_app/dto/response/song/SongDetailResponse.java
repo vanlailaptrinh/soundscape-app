840b5a5fc8a330612fdba30e42f40cb98e72a2dd
Ha Truong <hacaotantruong0@gmail.com>
2026-05-23 21:00:10 +0700

    tao lop SongDetailResponse

---
+++ src/main/java/com/example/soundscape_app/dto/response/song/SongDetailResponse.java
@@ -0,0 +1,34 @@
+package com.example.soundscape_app.dto.response.song;
+
+import com.example.soundscape_app.enums.GenreEnum;
+import com.example.soundscape_app.enums.MediaEnum;
+import com.example.soundscape_app.enums.SongStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SongDetailResponse {
+    private Long id;
+    private String title;
+    private String imageUrl;
+    private String author;
+    private int playCount;
+    private LocalDateTime createdAt;
+    private List<GenreEnum> genres;
+    private MediaEnum type;
+    private Double ratingAvg;
+    private Integer ratingCount;
+    private SongStatusEnum status;
+
+    // Thông tin Auth (chỉ lấy tên)
+    private Long authId;
+    private String authUsername;
+}
